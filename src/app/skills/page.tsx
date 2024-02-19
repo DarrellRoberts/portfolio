48aa21f6582c49399df157ce4359d200890a2018
DarrellRoberts <mrdarrellroberts@gmail.com>
2024-02-19 13:48:43 +0100

    added skills section

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,7 +1,38 @@
+"use client"
+import {useReducer} from "react"
 import Navbar from "@/components/navbar/Navbar"
+import Image from "next/image"
 import styles from "./skills.module.css"
 
+type StateType = {
+    showFrontend: boolean;
+    showBackend: boolean;
+    showDatabase: boolean;
+    showTools: boolean;
+  }
+
+const reducer = (state: StateType, action) => {
+    switch (action.type) {
+        case "toggleShowFrontend":
+            return {showFrontend: !state.showFrontend}
+        case "toggleShowBackend":
+            return {showBackend: !state.showBackend}
+        case "toggleShowDatabase":
+            return {showDatabase: !state.showDatabase}
+        case "toggleShowTools":
+            return {showTools: !state.showTools}
+        default:
+            return state
+    }
+}
+
 export default function About() {
+const [state, dispatch] = useReducer(reducer, {
+    showFrontend: false,
+    showBackend: false,
+    showDatabase: false,
+    showTools: false,
+})
     return (
         <>
     <Navbar />
@@ -10,39 +41,89 @@ export default function About() {
           <div className={styles.darrellCon}>
             <h1 className="heroTitle">Skills</h1>
             <div className={styles.skillsCon}>
-                <h2> Frontend </h2>
+
+                <h2 onClick={() => dispatch({type: "toggleShowFrontend"})}> 
+                Frontend 
+                </h2>
+                {state.showFrontend ? (
                 <ul className={styles.skillsList}>
-                    <li>JavaScript</li>
-                    <li>TypeScript</li>
                     <li>HTML5</li>
                     <li>CSS3</li>
+                    <li>TailwindCSS</li>
+                    <li>JavaScript</li>
+                    <li>TypeScript</li>
                     <li>React</li>
                     <li>Next</li>
-                </ul>
-                <h2> Backend </h2>
+                    <li>Vite</li>
+                    <li>Vercel</li>
+                    <li>Netlify</li>
+                    <li>Three.js</li>
+                </ul>) : (                
+                <ul className={styles.skillsListOut}>
+                    <li>HTML5</li>
+                    <li>CSS3</li>
+                    <li>TailwindCSS</li>
+                    <li>JavaScript</li>
+                    <li>TypeScript</li>
+                    <li>React</li>
+                    <li>Next</li>
+                    <li>Vite</li>
+                    <li>Vercel</li>
+                    <li>Netlify</li>
+                    <li>Three.js</li>
+                </ul>)}
+
+                <h2 onClick={() => dispatch({type: "toggleShowBackend"})}> 
+                Backend 
+                </h2>
+                {state.showBackend ? (
                 <ul className={styles.skillsList}>
                     <li>Node.js</li>
                     <li>Express.js</li>
-                    <li>Java</li>
-                </ul>
-                <h2> Database </h2>
+                    <li>Render</li>
+                </ul> ) : (                
+                <ul className={styles.skillsListOut}>
+                    <li>Node.js</li>
+                    <li>Express.js</li>
+                    <li>Render</li>
+                </ul>)}
+
+                <h2 onClick={() => dispatch({type: "toggleShowDatabase"})}> 
+                Database 
+                </h2>
+                {state.showDatabase ? (
                 <ul className={styles.skillsList}>
-                    <li>SQL</li>
-                    <li>MySQL</li>
                     <li>MongoDB</li>
-                    <li>PostresSQL</li>
-                </ul>
-                <h2> Tools </h2>
+                    <li>MySQL</li>
+                    <li>PostgreSQL</li>
+                </ul> ) : (                
+                <ul className={styles.skillsListOut}>
+                    <li>MongoDB</li>
+                    <li>MySQL</li>
+                    <li>PostgreSQL</li>
+                </ul>)}
+
+                <h2 onClick={() => dispatch({type: "toggleShowTools"})}> 
+                Tools 
+                </h2>
+                {state.showTools ? (
                 <ul className={styles.skillsList}>
-                    <li>Docker</li>
                     <li>Git</li>
+                    <li>Docker</li>
                     <li>Gimp</li>
-                    <li>Three.js</li>
-                </ul>
+                    <li>Figma</li>
+                </ul>) : (                
+                <ul className={styles.skillsListOut}>
+                    <li>Git</li>
+                    <li>Docker</li>
+                    <li>Gimp</li>
+                    <li>Figma</li>
+                </ul>)}
             </div>
           </div>
         </div>
       </section>
+      <Image className={styles.cog} src="/cog.png" alt="cog" width="400" height="400" />
         </>
     )
 }
