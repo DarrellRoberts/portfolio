88543d50364dcf18c49ce596f7e2b4eb9d8e0649
DarrellRoberts <mrdarrellroberts@gmail.com>
2024-05-15 14:58:05 +0200

    testing redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {  
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'i.imgur.com',
-            port: '',
-          }
-        ]   
-}
-}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+        port: '',
+      },
+    ],
+  },
+  redirects: [
+    {
+      source: '/blog/1',
+      destination: '/blog/should-web-developers-learn-wordpress',
+      permanent: true,
+    },
+  ],
+};
 
 export default nextConfig;
