fb87a27efad1c0b34d1749da9b6995f94e39067e
DarrellRoberts <mrdarrellroberts@gmail.com>
2024-07-15 17:51:19 +0200

    00025: added searchbar to blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,15 +1,23 @@
 "use client";
 import styles from "./blog.module.css";
 import EntriesArray from "../../components/blog-assets/EntriesArray";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ThemeContext } from "@/components/context/ThemeContext";
 import Link from "next/link";
 import Image from "next/image";
+import SearchBar from "@/components/search/SearchBar";
 
 export default function Blog() {
+  const [searchbar, setSearchBar] = useState<string>("");
   const { light, dark, isLightTheme } = useContext(ThemeContext);
   const themeStyles = isLightTheme ? light : dark;
 
+  let blogArray = [];
+  if (searchbar) {
+    blogArray = EntriesArray.filter((text) => text.path.includes(searchbar));
+  } else {
+    blogArray = EntriesArray;
+  }
   return (
     <div style={{ backgroundColor: themeStyles.bg }} className="">
       <section
@@ -25,33 +33,42 @@ export default function Blog() {
               Blog
             </h1>
             <div className={styles.postsCon}>
-              {EntriesArray.map((item, i) => (
-                <Link key={i} href={`/blog/${item.path}`} 
-                style={{
-                  borderBottomColor: themeStyles.secondaryText,
-                  borderBottomWidth: "1px"}}>
-                  <div className={styles.posts}>
-                    <h2 style={{ color: themeStyles.mainText }}>
-                      {item.title}
-                    </h2>
-                    <h3 style={{ color: themeStyles.secondaryText}}>
-                      Posted: {item.date}
-                    </h3>
-                  </div>
-                                      <div
-                    style={{ 
-                      borderColor: themeStyles.secondaryText, 
-                      backgroundImage: `url(${item.images})`,
-                      backgroundSize: "cover",
-                      backgroundPosition: "center",
-                      backgroundRepeat: "no-repeat",
-                    
+              <SearchBar setSearchBar={setSearchBar} />
+              {blogArray.length !== 0 ? (
+                blogArray.map((item, i) => (
+                  <Link
+                    key={i}
+                    href={`/blog/${item.path}`}
+                    style={{
+                      borderBottomColor: themeStyles.secondaryText,
+                      borderBottomWidth: "1px",
                     }}
-                    className={styles.rotateBorder}
                   >
-                  </div>
-                </Link>
-              ))}
+                    <div className={styles.posts}>
+                      <h2 style={{ color: themeStyles.mainText }}>
+                        {item.title}
+                      </h2>
+                      <h3 style={{ color: themeStyles.secondaryText }}>
+                        Posted: {item.date}
+                      </h3>
+                    </div>
+                    <div
+                      style={{
+                        borderColor: themeStyles.secondaryText,
+                        backgroundImage: `url(${item.images})`,
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                        backgroundRepeat: "no-repeat",
+                      }}
+                      className={styles.rotateBorder}
+                    ></div>
+                  </Link>
+                ))
+              ) : (
+                <div className={styles.noresultsCon}>
+                  <h2 style={{ color: themeStyles.mainText }}>No results</h2>
+                </div>
+              )}
             </div>
           </div>
           <Image
---
+++ src/components/search/SearchBar.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { Input, Space } from "antd";
+import type { GetProps } from "antd";
+import { useState, useContext } from "react";
+import { ThemeContext } from "@/components/context/ThemeContext";
+import "./searchbar.css";
+
+type SearchProps = GetProps<typeof Input.Search>;
+
+const { Search } = Input;
+
+export default function SearchBar({setSearchBar}) {
+  const [inputValue, setValue] = useState("");
+  const { light, dark, isLightTheme } = useContext(ThemeContext);
+  const themeStyles = isLightTheme ? light : dark;
+
+  const onSearch: SearchProps["onSearch"] = () => {
+    setValue("");
+  }
+
+  const handleInputChange = (e) => {
+    setValue(e.target.value.toLowerCase().replace(/\s+/g, '-'));
+    setSearchBar(e.target.value.toLowerCase().replace(/\s+/g, '-'));
+  };
+
+  return (
+    <Space>
+      <Search
+      className={isLightTheme ? "searchCon" : "darkSearchCon"}
+      placeholder="Search here by title" 
+      onSearch={onSearch}
+      onChange={handleInputChange}
+      enterButton="Search"
+      allowClear={true}
+      size="large"
+      />
+    </Space>
+  );
+}
