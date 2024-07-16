559f091b54218e0182d96adb0016eaa283ca89ef
DarrellRoberts <mrdarrellroberts@gmail.com>
2024-07-16 17:02:08 +0200

    00025: added next and prev nav to blog

--- src/app/blog/[path]/page.tsx
+++ src/app/blog/[path]/page.tsx
@@ -4,7 +4,7 @@ import EntriesArray from "../../../components/blog-assets/EntriesArray";
 import styles from "./post.module.css";
 import { useContext } from "react";
 import { ThemeContext } from "@/components/context/ThemeContext";
-import { useParams } from "next/navigation"; 
+import { useParams } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -14,10 +14,19 @@ type ParamObject = {
 
 export default function BlogPost() {
   const params: ParamObject = useParams();
-  const post = EntriesArray?.find(heading => heading?.path === params?.path);
+  const post = EntriesArray?.find((heading) => heading?.path === params?.path);
   const { light, dark, isLightTheme } = useContext(ThemeContext);
   const themeStyles = isLightTheme ? light : dark;
 
+  const articleIndex1 =
+    EntriesArray.indexOf(post) < EntriesArray.length - 1
+      ? EntriesArray.indexOf(post) + 1
+      : null;
+  const articleIndex2 =
+    EntriesArray.indexOf(post) !== 0 ? EntriesArray.indexOf(post) - 1 : null;
+
+  console.log(articleIndex1);
+  console.log(articleIndex2);
   return (
     <div style={{ backgroundColor: themeStyles.bg }}>
       <section
@@ -25,21 +34,19 @@ export default function BlogPost() {
         className={styles.homepageCon}
       >
         <div className={styles.titleCon}>
-          <span style={{ color: themeStyles.mainText, fontStyle: "italic"}}>
+          <span style={{ color: themeStyles.mainText, fontStyle: "italic" }}>
             By Darrell Roberts
           </span>
           <span style={{ color: themeStyles.mainText }}>
             Posted: {post?.date}
           </span>
-          <h1
-            style={{ color: themeStyles.secondaryText }}
-          >
-            {post?.title}
-          </h1>
+          <h1 style={{ color: themeStyles.secondaryText }}>{post?.title}</h1>
         </div>
-        <div className={styles.imageCon} >
+        <div className={styles.imageCon}>
           <Image
-            style={{ boxShadow: "0px 0px 20px 5px" + themeStyles.secondaryText }}
+            style={{
+              boxShadow: "0px 0px 20px 5px" + themeStyles.secondaryText,
+            }}
             src={post?.images[0]}
             alt={post?.imageAlt[0]}
             width="850"
@@ -49,11 +56,41 @@ export default function BlogPost() {
             {post?.imageCaption[0]}
           </span>
         </div>
-        <div className={isLightTheme ? styles.articleCon : styles.articleConDark}>
-            {post?.article}
+        <div
+          className={isLightTheme ? styles.articleCon : styles.articleConDark}
+        >
+          {post?.article}
         </div>
-        <Link style={{ color: themeStyles.secondaryText }} href="/blog" className="ml-5">
-          Back to Blog homepage</Link>
+        <div className={styles.nextArticleCon}>
+          {articleIndex1 ? (
+            <Link href={`/blog/${EntriesArray[articleIndex1]?.path}`}>
+              <div className={styles.prevArticle}>
+                <h2 style={{ color: themeStyles.mainText }} className="underline">Previous</h2>
+                <h3 style={{ color: themeStyles.mainText }}>
+                {EntriesArray[articleIndex1]?.title}
+                </h3>
+              </div>
+            </Link>
+          ) : <div></div>}
+          {articleIndex2 || EntriesArray.indexOf(post) === 1 ? (
+            <Link href={`/blog/${EntriesArray[articleIndex2]?.path}`}>
+              <div className={styles.nextArticle}>
+                <h2 style={{ color: themeStyles.mainText }} className="underline">Next</h2>
+                <h3 style={{ color: themeStyles.mainText }}>
+                {EntriesArray[articleIndex2]?.title}
+                </h3>
+              </div>
+            </Link>
+          ) : null}
+        </div>
+        <br/>
+        <Link
+          style={{ color: themeStyles.secondaryText }}
+          href="/blog"
+          className="ml-5"
+        >
+          Back to Blog homepage
+        </Link>
       </section>
     </div>
   );
