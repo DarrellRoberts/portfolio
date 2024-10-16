e7b5a17a3ed5de7c294e0facfa192be87832e2e3
DarrellRoberts <mrdarrellroberts@gmail.com>
2024-10-16 18:28:18 +0200

    00036: added label to DM checkbox for better access

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import styles from "./navbar.module.css";
-import DarkMode from "./DarkMode";
+import DarkMode from "../dark-mode/DarkMode";
 
 export default function NavBar() {
   return (
