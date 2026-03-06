6b7926eb6ffae141f7b4d0673331dff9f70adf15
DarrellRoberts <mrdarrellroberts@gmail.com>
2026-03-06 17:40:18 +0100

    adds need4deed cont

--- src/data/openSource.ts
+++ src/data/openSource.ts
@@ -6,12 +6,14 @@ export const openSourceEN: OpenSource[] = [
     owner: "@need4deed",
     tech: "Next",
     feature: [
+      "Created Volunteer section for Refugee Accommodation Centres",
       "Created Agent contact details for Refugee Accommodation Centres",
       "Redirect users to their role-specific route after successful login",
       "Restyled availability grid according to figma wireframe",
       "Restyled and refactored language selector for volunteer form",
     ],
     githubLink: [
+      "https://github.com/need4deed-org/fe/pull/242",
       "https://github.com/need4deed-org/fe/pull/228",
       "https://github.com/need4deed-org/fe/pull/191",
       "https://github.com/need4deed-org/fe/pull/105",
@@ -88,12 +90,14 @@ export const openSourceDE: OpenSource[] = [
     owner: "@need4deed",
     tech: "Next",
     feature: [
+      "Bereich für Freiwillige für Flüchtlingsunterkünfte erstellt",
       "Kontaktdaten für Agenten in Unterkünften für Geflüchtete erstellt",
       "Weiterleitung der Benutzer zu ihrer rollenspezifischen Route nach erfolgreichem Login",
       "Verfügbarkeitsraster gemäß Figma-Wireframe neugestaltet",
       "Überarbeiteter und neu strukturierter Sprachwähler für das Freiwilligenformular",
     ],
     githubLink: [
+      "https://github.com/need4deed-org/fe/pull/242",
       "https://github.com/need4deed-org/fe/pull/228",
       "https://github.com/need4deed-org/fe/pull/191",
       "https://github.com/need4deed-org/fe/pull/105",
