2ad74f1aff6cec86251824c956aadaf1c3c1e461
DarrellRoberts <mrdarrellroberts@gmail.com>
2026-03-23 22:59:14 +0100

    adds uicard, removes hero card & fixes hero perf issues

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -7,7 +7,6 @@ import HeroWaves from "./HeroWaves"
 import "./hero.css"
 import HeroTumbleweed from "./HeroTumbleweed"
 import { useI18n } from "../../../locales/client"
-// test pipeline
 
 const Hero = () => {
   const [startAnimation, setStartAnimation] = useState<boolean>(false)
@@ -29,53 +28,69 @@ const Hero = () => {
         <div className="w-200 max-md:w-full aspect-[1/0.7] max-xl:aspect-[1/0.5] max-md:aspect-auto relative">
           <Image
             data-testid="hero-image"
-            src={isDark ? "/hero-light.webp" : "/hero-dark.webp"}
+            src="/hero-light.webp"
             fill
             alt="darrell_roberts"
-            fetchPriority="high"
-            className="object-contain"
+            priority
+            style={{
+              opacity: isDark ? "1" : "0",
+              zIndex: isDark ? "10" : "0",
+            }}
+            className="object-contain transition-opacity duration-100"
             sizes="(max-width: 850px) 100vw, 1600px"
-            loading="eager"
+          />
+          <Image
+            data-testid="hero-image"
+            src="/hero-dark.webp"
+            fill
+            alt="darrell_roberts"
+            priority
+            style={{ opacity: isDark ? "0" : "1" }}
+            className="object-contain transition-opacity duration-100"
+            sizes="(max-width: 850px) 100vw, 1600px"
           />
         </div>
         <h1 className="text-3xl self-center">{t("hero.frontend")}</h1>
       </div>
-      {isDark ? (
-        <div className="flex justify-center items-center max-md:items-baseline max-md:h-50 relative">
-          <div className="flex flex-col max-sm:w-full w-50 absolute max-md:bottom-auto">
-            <Image
-              onClick={sinkShip}
-              className={
-                startAnimation
-                  ? "sunken-ship-class w-50 h-50 z-1 absolute bottom-[37%] max-sm:right-1/4 right-[1%]"
-                  : "ship-class w-50 h-50 z-1 absolute bottom-[37%] max-sm:right-1/4 right-[1%] cursor-pointer"
-              }
-              src="/ship_dark.webp"
-              alt="ship"
-              width={500}
-              height={500}
-            />
-            <div className="flex w-full justify-center h-100 z-0">
-              <HeroWaves />
-            </div>
+      <div
+        className="flex justify-center items-center max-md:items-baseline max-md:h-50 relative transition-opacity duration-300"
+        style={{ opacity: isDark ? "1" : "0", zIndex: isDark ? "10" : "0" }}
+      >
+        <div className="flex flex-col max-sm:w-full w-50 absolute max-md:bottom-auto">
+          <Image
+            onClick={sinkShip}
+            className={
+              startAnimation
+                ? "sunken-ship-class w-50 h-50 z-1 absolute bottom-[37%] max-sm:right-1/4 right-[1%]"
+                : "ship-class w-50 h-50 z-1 absolute bottom-[37%] max-sm:right-1/4 right-[1%] cursor-pointer"
+            }
+            src="/ship_dark.webp"
+            alt="ship"
+            width={500}
+            height={500}
+          />
+          <div className="flex w-full justify-center h-100 z-0">
+            <HeroWaves />
           </div>
         </div>
-      ) : (
-        <div className="flex justify-center relative max-sm:h-50">
-          <div className="flex flex-col min-md:justify-start w-50 cursor-pointer min-md:absolute min-md:bottom-1/2">
-            <Image
-              data-testid="cactus"
-              onClick={sinkShip}
-              className="w-50 h-50 z-1 min-md:absolute min-md:bottom-[37%] min-md:right-[5%]"
-              src={"/cactus_light.webp"}
-              alt={"cactus"}
-              width={500}
-              height={500}
-            />
-          </div>
-          {startAnimation && <HeroTumbleweed />}
+      </div>
+      <div
+        className="flex justify-center relative max-sm:h-50 transition-opacity duration-300"
+        style={{ opacity: isDark ? "0" : "1", zIndex: isDark ? "0" : "10" }}
+      >
+        <div className="flex flex-col min-md:justify-start w-50 cursor-pointer min-md:absolute min-md:bottom-1/2">
+          <Image
+            data-testid="cactus"
+            onClick={sinkShip}
+            className="w-50 h-50 z-1 min-md:absolute min-md:bottom-[37%] min-md:right-[5%]"
+            src={"/cactus_light.webp"}
+            alt={"cactus"}
+            width={500}
+            height={500}
+          />
         </div>
-      )}
+        {startAnimation && <HeroTumbleweed />}
+      </div>
     </div>
   )
 }
--- src/components/ui/UiCard.tsx
+++ src/components/ui/UiCard.tsx
@@ -1,3 +1,94 @@
-export const UiCard = () => {
-  return <div>UiCard</div>
+"use client"
+
+import { Dispatch, ReactNode, useContext } from "react"
+import Image from "next/image"
+import { DarkContext } from "@/context/DarkContext"
+
+type DataObject = {
+  title: string
+  skillsImages: string[]
+  skillsLabels: string[]
 }
+
+type Props = {
+  dataObject: DataObject
+  setImage: Dispatch<React.SetStateAction<string | null>>
+  setLabel: Dispatch<React.SetStateAction<string | null>>
+}
+
+export const UiCard = ({ dataObject, setImage, setLabel }: Props) => {
+  const { isDark } = useContext(DarkContext)
+
+  const Container = UiCard.Container
+  const Header = UiCard.Header
+  const Body = UiCard.Body
+
+  return (
+    <Container isDark={isDark}>
+      <Header title={dataObject.title} />
+      <Body setImage={setImage} setLabel={setLabel} dataObject={dataObject} />
+    </Container>
+  )
+}
+
+type UiCardProps = {
+  children: ReactNode
+  title: string
+  setImage: Dispatch<React.SetStateAction<string | null>>
+  setLabel: Dispatch<React.SetStateAction<string | null>>
+  dataObject: DataObject
+  isDark: boolean
+}
+
+UiCard.Container = ({
+  children,
+  isDark,
+}: Pick<UiCardProps, "children" | "isDark">) => (
+  <div
+    style={{ background: isDark ? "#18181B" : "#ffffff" }}
+    className="py-4 h-75 max-xl:h-65 max-sm:h-75 aspect-[18/20] z-0 overflow-visible relative rounded-2xl"
+  >
+    {children}
+  </div>
+)
+
+UiCard.Header = ({ title }: Pick<UiCardProps, "title">) => (
+  <div className="pb-0 pt-2 px-4 flex-col items-start">
+    <h2 className="font-bold text-large text-primary">{title}</h2>
+  </div>
+)
+
+UiCard.Body = ({
+  setImage,
+  setLabel,
+  dataObject,
+}: Omit<UiCardProps, "children" | "title" | "isDark">) => (
+  <div className="grid grid-cols-3 gap-2 overflow-visible">
+    {dataObject.skillsImages.map((skillImage, index) => (
+      <div
+        className="relative inline-display cursor-pointer group overflow-visible flex items-center justify-center p-4 hover:p-1 transition-all transition-discrete h-15"
+        key={dataObject.skillsLabels[index]}
+        onMouseEnter={() => {
+          setImage(skillImage)
+          setLabel(dataObject.skillsLabels[index])
+        }}
+        onMouseLeave={() => {
+          setImage(null)
+          setLabel(null)
+        }}
+      >
+        <div className="flex items-center justify-center max-sm:w-auto">
+          <Image
+            width={50}
+            height={50}
+            alt={dataObject.skillsLabels[index]}
+            src={skillImage}
+          />
+        </div>
+        <span className="opacity-0 w-30 bg-secondary text-primary text-center rounded-2xl absolute z-1000 group-hover:opacity-100 transition-all transition-discrete border-1 border-primary top-[90%] max-sm:block min-sm:hidden">
+          {dataObject.skillsLabels[index]}
+        </span>
+      </div>
+    ))}
+  </div>
+)
