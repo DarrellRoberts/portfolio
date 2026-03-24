d0eacf4955f0d49427477292a5d5ee3bed323e8b
DarrellRoberts <mrdarrellroberts@gmail.com>
2026-03-24 12:21:28 +0100

    fixes dm toggle bug

--- src/components/misc/DarkMode.tsx
+++ src/components/misc/DarkMode.tsx
@@ -1,15 +1,29 @@
 "use client"
 
 import { DarkContext } from "@/context/DarkContext"
-import React, { useContext } from "react"
+import React, { useContext, useEffect, useRef } from "react"
 
 const DarkMode = () => {
   const { toggleDark } = useContext(DarkContext)
+  const dmCheckbox = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    const darkQuery = window.matchMedia("(prefers-color-scheme: dark)")
+    if (darkQuery.matches) {
+      if (dmCheckbox?.current) {
+        dmCheckbox.current.checked = true
+      }
+    }
+  }, [dmCheckbox])
   return (
-    <div
-      className="rounded-full bg-primary p-5 cursor-pointer hover:p-6 transition-all transition-discrete"
-      onClick={toggleDark}
-    />
+    <div className="dark-mode-container">
+      <input
+        type="checkbox"
+        id="dark-mode-checkbox"
+        ref={dmCheckbox}
+        onClick={toggleDark}
+      />
+    </div>
   )
 }
 
